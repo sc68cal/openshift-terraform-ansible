40c3623bf13d2d723a9bb655da9bee02cafa2659
Sean M. Collins <sean@coreitpro.com>
2018-09-11 16:30:28 -0400

    Add an IPv6 ssh rule

--- openstack/main.tf
+++ openstack/main.tf
@@ -22,6 +22,16 @@ resource "openstack_networking_secgroup_rule_v2" "port_22" {
   security_group_id = "${openstack_networking_secgroup_v2.os3-sec-group.id}"
 }
 
+resource "openstack_networking_secgroup_rule_v2" "port_22_v6" {
+  direction = "ingress"
+  ethertype = "IPv6"
+  port_range_min = 22
+  port_range_max = 22
+  protocol = "tcp"
+  remote_ip_prefix = "::/0"
+  security_group_id = "${openstack_networking_secgroup_v2.os3-sec-group.id}"
+}
+
 resource "openstack_networking_secgroup_rule_v2" "port_53" {
   direction = "ingress"
   ethertype = "IPv4"
